f04171770ee4c9ef76bec3f337e92c76303a4430
Aditya Deshmukh <vdaditya.57@gmail.com>
2023-03-16 17:47:34 +0530

    Changes added!

---
+++ product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    double a, b, product;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    // Calculating product
+    product_nums = a * b;
+
+    // %.2lf displays number up to 2 decimal point
+    printf("Product = %.2lf", product_nums);
+    
+    return 0;
+}
+//this is my first file which i pushed on git hub
